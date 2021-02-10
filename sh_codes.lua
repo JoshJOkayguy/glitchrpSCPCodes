6cac4b1681df925d4334f8fb1049bde3b01d2464
JoshJOkayguy <55960406+JoshJOkayguy@users.noreply.github.com>
2021-02-10 08:30:08 -0500

    Update sh_codes.lua

--- sh_codes.lua
+++ sh_codes.lua
@@ -1,22 +1,20 @@
--- Use this table to add new code materials.
-codeMaterials = {
-["defcon5"] = "materials/icon16/shading.png",
-} 
-
 -- Use this table to allow jobs to use the command.
 allowedJobs = {
 ["Citizen"] = true,
 
 }
 
+-- (DO NOT EDIT THIS IF YOU DO NOT KNOW WHAT YOU ARE DOING) Use this table to add new code materials.
+codeMaterials = {
+["defcon5"] = "materials/icon16/shading.png",
+} 
+
 if SERVER then
 
 	for k,v in ipairs(codeMaterials) do
-		if codeMaterials then
+		
+		resource.AddFile(v)
 
-			resource.AddFile(k)
-	
-		end
 	end
 
 end
@@ -30,7 +28,7 @@ if CLIENT then
 		
 		local playerInput = string.Explode( " ", strText ) 
 		
-		if playerInput[1] != "!code" then
+		if playerInput[1] != "!code" or bDead then
 			return
 		else
 			return true
@@ -52,6 +50,10 @@ if CLIENT then
 					print(strText)	
 				elseif playerInput[2] == "purple" then
 					print(strText)
+				else
+					chat.AddText("Arguments: defcon5, defcon4, defcon3, defcon2, defcon1, white, purple, silver")
+					chat.PlaySound()
+				end
 			else
 				chat.AddText("You don't have permission to use this command!")
 				chat.PlaySound()
